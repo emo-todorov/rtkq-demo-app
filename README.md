27054bfcafce7b8b6bc379b26a76ae6a81e5b000
emo-todorov <emiliyantodorov89@gmail.com>
2024-12-18 17:09:17 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+App made with React and RTKQ for learning purposes
---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="main-header">
+        <div>
+          <span className="users-label">Users</span>
+          <button>Add user</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
